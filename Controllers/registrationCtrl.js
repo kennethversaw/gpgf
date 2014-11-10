49e02aa5437d8766bbbc93e35216b0b708076e06
kennethversaw <kennethversaw@gmail.com>
2014-11-09 18:02:06 -0600

    Added Modal box and paypal button

---
+++ Controllers/registrationCtrl.js
@@ -0,0 +1,59 @@
+﻿myApp.controller('registrationCtrl', function ($scope, Registration, $modal, $log) {
+
+    $scope.newRegistration = new Registration();
+    // $scope.items = ['item1', 'item2', 'item3'];
+    $scope.showProgress = false;
+    $scope.register = function () {    
+
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: ModalInstanceCtrl,
+                size: 'lg',
+                resolve: {
+                    newRegistration: function () {
+                        return $scope.newRegistration;
+                    }
+                }
+            });
+
+        //$scope.showProgress = true;
+        //// call the service         
+        //$scope.newRegistration.$save(function () {
+        //    // when saved, reload the list and recreate a new task 
+
+        //    var modalInstance = $modal.open({
+        //        templateUrl: 'myModalContent.html',
+        //        controller: ModalInstanceCtrl,
+        //        size: 'lg',
+        //        resolve: {
+        //            newRegistration: function () {
+        //                return $scope.newRegistration;
+        //            }
+        //        }
+        //    });
+
+        //    modalInstance.result.then(function (selectedItem) {
+        //        // $scope.selected = selectedItem;
+        //    }, function () {
+        //        $log.info('Modal dismissed at: ' + new Date());
+        //    });
+        //    $scope.newRegistration = new Registration();
+        //    $scope.showProgress = false;
+        //});
+
+    }
+    var ModalInstanceCtrl = function ($scope, $modalInstance, newRegistration) {
+        $scope.JeresyFee = (newRegistration.needJeresy == true) ? 18 : 0;
+        $scope.ButtonCost = (isNaN(newRegistration.numberofButtons)) ? 0 : newRegistration.numberofButtons * 5;
+        $scope.Donation = (newRegistration.yesDonate == true) ? 5 : 0;
+        $scope.Total = 25 + $scope.JeresyFee + $scope.ButtonCost + $scope.Donation;
+
+        $scope.ok = function () {
+            $modalInstance.dismiss('ok');
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
+});
---
+++ app.js
@@ -0,0 +1,27 @@
+﻿
+var myApp = angular.module('GPGF', ['ngResource', 'ui.bootstrap', 'ngRoute']).service('Registration', function ($resource) {
+    return $resource('https://greatplainsgamefestival.azure-mobile.net/Tables/registration/', { signupId: '@id' },
+        {
+            'update': { method: 'PATCH' }
+        });
+});
+
+myApp.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common['X-ZUMO-APPLICATION'] = 'tjwRUrjhmjrzhDNTNOyGQdvRUnyqaU13';
+    $httpProvider.defaults.headers.common['Content-Type'] = 'Application/json';
+
+}]);
+
+
+//.config(function ($routeProvider) {
+//    $routeProvider
+//        .when('/register', {
+//            templateUrl: 'Views/registration.html',
+//            controller: 'registrationCtrl'
+//        })
+//      .when('/', {
+//          templateUrl: 'Views/home.html',
+//          controller: ''
+//      })
+
+//});
