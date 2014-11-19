4bc5824ed59c72fc6420ba61dfb1737f1436de6c
kennethversaw <kennethversaw@gmail.com>
2014-11-18 19:16:10 -0600

    Registration validation
    Added validation to registration

--- Controllers/registrationCtrl.js
+++ Controllers/registrationCtrl.js
@@ -3,18 +3,22 @@
     $scope.newRegistration = new Registration();
     // $scope.items = ['item1', 'item2', 'item3'];
     $scope.showProgress = false;
-    $scope.register = function () {    
-
-            var modalInstance = $modal.open({
-                templateUrl: 'myModalContent.html',
-                controller: ModalInstanceCtrl,
-                size: 'lg',
-                resolve: {
-                    newRegistration: function () {
-                        return $scope.newRegistration;
-                    }
-                }
-            });
+    $scope.register = function () {
+        $scope.newRegistration.$save();
+    }
+        //$scope.newRegistration.$save(function() {
+           
+        //});
+            //var modalInstance = $modal.open({
+            //    templateUrl: 'myModalContent.html',
+            //    controller: ModalInstanceCtrl,
+            //    size: 'lg',
+            //    resolve: {
+            //        newRegistration: function () {
+            //            return $scope.newRegistration;
+            //        }
+                //}
+          
 
         //$scope.showProgress = true;
         //// call the service         
@@ -41,19 +45,19 @@
         //    $scope.showProgress = false;
         //});
 
-    }
-    var ModalInstanceCtrl = function ($scope, $modalInstance, newRegistration) {
-        $scope.JeresyFee = (newRegistration.needJeresy == true) ? 18 : 0;
-        $scope.ButtonCost = (isNaN(newRegistration.numberofButtons)) ? 0 : newRegistration.numberofButtons * 5;
-        $scope.Donation = (newRegistration.yesDonate == true) ? 5 : 0;
-        $scope.Total = 25 + $scope.JeresyFee + $scope.ButtonCost + $scope.Donation;
+   // }
+    //var ModalInstanceCtrl = function ($scope, $modalInstance, newRegistration) {
+    //    $scope.JeresyFee = (newRegistration.needJeresy == true) ? 18 : 0;
+    //    $scope.ButtonCost = (isNaN(newRegistration.numberofButtons)) ? 0 : newRegistration.numberofButtons * 5;
+    //    $scope.Donation = (newRegistration.yesDonate == true) ? 5 : 0;
+    //    $scope.Total = 25 + $scope.JeresyFee + $scope.ButtonCost + $scope.Donation;
 
-        $scope.ok = function () {
-            $modalInstance.dismiss('ok');
-        };
+    //    $scope.ok = function () {
+    //        $modalInstance.dismiss('ok');
+    //    };
 
-        $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
-        };
-    };
+    //    $scope.cancel = function () {
+    //        $modalInstance.dismiss('cancel');
+    //    };
+    //};
 });
